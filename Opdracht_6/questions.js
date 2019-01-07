f0401dec02878a3b7034331f3b7a3f9b7b652cc9
Mariia <mariiaolden@gmail.com>
2019-01-07 11:47:32 +0100

    opdracht 6 js done

--- Opdracht_6/questions.js
+++ Opdracht_6/questions.js
@@ -1,64 +1,72 @@
 
 // Create a array containing "Html", "CSS", "Java", "PHP"
 var CreationArrayLanguages = function () {
-
+  return ["Html","CSS","Java","PHP"];
 }
 
 // Create a array containing the numbers from 0 to 5
 var CreateArrayNumbers = function () {
-
+  return [0,1,2,3,4,5];
 }
 
 // Replace the third element of the array with "Javascript"
 var languages = ["Html","CSS","Java","PHP"];
 var ReplacementElement = function (languages) {
-
+  languages[2] = "Javascript";
+  return languages;
 }
 
 // Add "Ruby" and "Python" at the end of the array
 var languages = ['Html', 'CSS', 'Javascript', 'PHP'];
 var AddElementLanguages = function (languages) {
-
+  languages.push("Ruby","Python");
+  return languages;
 }
 
 // Add "-2" and "-1" at the beginning of the table numbers
 var numbers = [0,1,2,3,4,5];
 var AddElementNumbers = function (numbers) {
-
+var num = [-2,-1];
+var all = num.concat(numbers);
+return all;
 }
 
 // Delete the first element of the languages array
 var languages = ['Html', 'CSS', 'Javascript', 'PHP', 'Ruby', 'Python'];
 var DeletingFirstElement = function (languages) {
-
+  languages.shift();
+  return languages;
 }
 
 // Delete the last element of the languages array
 var languages = ['CSS', 'Javascript', 'PHP', 'Ruby', 'Python'];
 var DeleteLastElement = function (languages) {
-
+  languages.pop();
+  return languages;
 }
 
 // Make the "social_networks_chain" chain into a "social_networks" array
 var social_networks_chain = 'Facebook,Twitter,Google +,Viadeo,LinkedIn';
 var ArrayStringConversion = function (social_networks_chain) {
-
+var arr = social_networks_chain.split(",");
+  return arr;
 }
 
 // Make the "languages" array into a "languages_chain" string. Separate languages with a comma
 var languages = ['CSS', 'Javascript', 'PHP', 'Ruby'];
 var ChainArrayConversion = function (languages) {
-
+  return languages.toString();
 }
 
 // Sort the "social_networks" array in alphabetical order
 var social_networks = ['Facebook','Twitter', 'Google +','Viadeo','LinkedIn'];
 var SortingArray = function (social_networks) {
+  return social_networks.sort();
 
 }
 
 // Bonus: Reverse the "languages" array
 var languages = ['CSS','Javascript','PHP','Ruby'];
 var ReverseArray = function (languages){
-
+return languages.reverse();
 }
