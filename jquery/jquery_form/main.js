60e90977b20e13e312ee5a7e4f9f818c492536e7
Mariia <mariiaolden@gmail.com>
2019-01-11 16:23:46 +0100

    something corrected at jquery

--- jquery/jquery_form/main.js
+++ jquery/jquery_form/main.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
   $("#send").click(function(event){
     event.preventDefault();
-  let namecheck = /^[a-zA-Z-]+$/;
+  let error = false;
+  let namecheck = /^[a-zA-Z-]{5,}$/;
   let name = $("#display-name").val();
   if (namecheck.test(name)) {
     $("#display-name"). removeClass('error').addClass('good');
   }else{
     $("#display-name").addClass('error').removeClass('good');
+    error = true;
   }
   let emailcheck = /^[\w\-.+]+@[a-zA-Z0-9.-]+.[a-zA-z0-9]{2,4}$/;
 
@@ -15,6 +17,7 @@ $(document).ready(function() {
    $("#email"). removeClass('error').addClass('good');
  }else {
    $("#email").addClass('error').removeClass('good');
+   error = true;
  }
  let passwordcheck = /^[a-z0-9_-]{6,18}$/;
  let password = $("#pass").val();
@@ -22,13 +25,16 @@ $(document).ready(function() {
     $("#pass").removeClass('error').addClass('good');
    } else {
     $("#pass").addClass('error').removeClass('good');
+    error = true;
    }
 let confirm = $('#pass-confirm').val();
 if(confirm === password) {
   $("#pass-confirm").removeClass('error').addClass('good');
 } else {
    $("#pass-confirm").addClass('error').removeClass('good');
+   error = true;
 }
+$('#mistake').toggle(error);
  });
 
 });
